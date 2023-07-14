74d4a5a9ae5628f561c0b6425d896075f0492480
Silvia <silcasavola@gmail.com>
2023-07-14 15:55:41 +0200

    yut

--- intro.js
+++ intro.js
@@ -57,3 +57,27 @@ $("#about-container").addClass("closed")
 
 }
 })
+
+// Set the inactivity timeout duration in milliseconds (1 minute = 60000 milliseconds)
+var inactivityTimeout = 60000;
+
+// Variable to hold the timeout ID
+var timeoutId;
+
+// Function to reload the page
+function reloadPage() {
+  location.reload();
+}
+
+// Function to reset the inactivity timer
+function resetTimer() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(reloadPage, inactivityTimeout);
+}
+
+// Event listeners for mouse movement and keypress events
+document.addEventListener("mousemove", resetTimer);
+document.addEventListener("keypress", resetTimer);
+
+// Start the initial timer
+resetTimer();
